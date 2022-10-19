5e71cadc14817ddd5120601293723439bcef20c1
hasankr <hasankr@abv.bg>
2022-10-19 23:01:23 +0300

    Add project files.

---
+++ StockMarket/Stock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StockMarket
+{
+    public class Stock
+    {
+        public string CompanyName { get; set; }
+        public string Director { get; set; }
+        public decimal PricePerShare { get; set; }
+        public int TotalNumberOfShares { get; set; }
+        public decimal MarketCapitalization => PricePerShare * TotalNumberOfShares;
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            CompanyName = companyName;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumberOfShares;
+
+        }
+        public override string ToString()
+        {
+            return 
+                $"Company: {CompanyName}{Environment.NewLine}"+
+                $"Director: {Director}{Environment.NewLine}" +
+                $"Price per share: ${PricePerShare}{Environment.NewLine}" +
+                $"Market capitalization: ${ MarketCapitalization}";
+            
+        }
+    }
+}
